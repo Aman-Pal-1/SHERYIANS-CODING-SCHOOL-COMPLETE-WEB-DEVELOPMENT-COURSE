391ae831b1370c0ea808b3504336dfb3f4e5b17a
Uditya Pal <149376004+Aman-Pal-1@users.noreply.github.com>
2024-02-22 09:17:27 +0530

    level 1 complete

---
+++ JavaScript and level 2/JavaScript Learn What Matters/script.js
@@ -0,0 +1,96 @@
+// words vs keywords ✅
+// chacha ------this is a word because the interpreter not know about the chacha it is not reserved in js .
+// for ------this is a keyword & this is reserved in  js . 
+
+// variables and Constants ✅
+// code me koi bhi data store karne ke liye jiska use hota h use khte h variables.  
+// constant are not change lakin variables change hote h . 
+
+// const dulha = "Lab";
+// const dulhan = "Labby";
+
+// var const let ✅
+// var and const are i told you above
+// let is similar to var but let have 3 difference between let and var. 
+
+// Hoisting ✅
+// hoisting is that jese kisi variables and functions ko banaane se phle use kar sakte h .
+// variables and functions are hoisted which means their declaration is moved on the top of the code. 
+
+// jese 🔽🔽🔽 
+// console.log(a);
+// var a = 12;
+
+// undefined or not-defined me farak h 
+// the thing exist but we dont know about that thing is called undefined or h 
+// the thing not exists are not defined . thats it
+
+// types in js ✅
+// primitive or reference
+
+//primitives = number, string, null, undefined,boolean
+//aise koi bhi value jiska copy karne par real copy ho jai wo primitive value hoti h
+//for example var a = 12;
+
+//references = {}, [], ()
+//aise koi bhi value jisko copy krne per real copy nhi hota balki us main value ka reference pass ho jata h use hum reference value khete h 
+// for example var b = [1,2,3,4,5];
+
+// conditionals in js ✅
+// conditionals are - if , else , else-if 
+// jab bhi baat agar magar pe ayegi , ya fir baat aaygi aisa hua to ye kro wesa ho jai to wo karo means conditions lag jai 
+// there are two conditions in conditionals are - true , false  but jaruri nhi ki hum direct true or false likhe koi condition bhi ho sakti h jo true ho ya false ho . thats it
+// example - 
+// if (true){};
+// if (false){};
+// or 
+// if (12>10){}; it is a condition and its true 
+// if (1>5){}; it is a condition and its false 
+
+// if wala code jab nhi chalta tab else chalta h mean that when if is false the else work 
+// example of else-if -
+// if(11>12){
+
+// }
+// else if(12>13){
+
+// }
+// else if(13>14){
+
+// }
+// else{
+
+// }
+// loops in js ✅
+// there are 2 types of loop
+// for loop
+// while loop
+// loop ka matlab hota h repeat 
+// example - 
+// for(start;end;change);
+// 0 - 10                 hame zero se ten tak print kana h to loop 11 war chalta h 
+
+// for(var i = 0; i<11; i++){
+//     console.log(i);
+// }
+
+// ques - 25 to 50 tak print karna h 
+
+// for(var i = 25; i<51; i++){
+//     console.log(i);
+// }
+
+// ab aate h while loop ke taraf while loop ki kahani bilkul simple h conditions true ho ya false ho . thats it 
+
+// jese ki -
+// var a = 5;
+// while (a<22){
+//     a++;
+//     console.log(a)
+// }
+// functions in js ✅
+//functions mainly three kam ke hote h
+// fuctions ka matlab jab aap kucch code ko likh kar koi naam de sakte ho . 
+//  phla kaam jab aapka code aap turant nhi chalana chahate ho future me chalana chahate ho 
+// dusra kaam jab aapka code aap reuse karna chahate ho  
+// teesra jab aapka code aap chalana chahete ho har baar with different data 
